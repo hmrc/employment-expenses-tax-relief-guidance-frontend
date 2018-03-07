956ec96431f41d9cedddfc0010a223deba7a2cb8
Nick Wilson <thenickwilson@gmail.com>
2018-03-07 08:34:20 +0000

    Initial commit of template code

---
+++ .g8/optionsPage/app/controllers/$className$Controller.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import connectors.DataCacheConnector
+import controllers.actions._
+import config.FrontendAppConfig
+import forms.$className$FormProvider
+import identifiers.$className$Id
+import models.$className$
+import utils.{Enumerable, Navigator, UserAnswers}
+import views.html.$className;format="decap"$
+
+import scala.concurrent.Future
+
+class $className$Controller @Inject()(
+                                        appConfig: FrontendAppConfig,
+                                        override val messagesApi: MessagesApi,
+                                        dataCacheConnector: DataCacheConnector,
+                                        navigator: Navigator,
+                                        getData: DataRetrievalAction,
+                                        requireData: DataRequiredAction,
+                                        formProvider: $className$FormProvider) extends FrontendController with I18nSupport with Enumerable.Implicits {
+
+  val form = formProvider()
+
+  def onPageLoad() = (getData andThen requireData) {
+    implicit request =>
+      val preparedForm = request.userAnswers.$className;format="decap"$ match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+      Ok($className;format="decap"$(appConfig, preparedForm))
+  }
+
+  def onSubmit() = (getData andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest($className;format="decap"$(appConfig, formWithErrors))),
+        (value) =>
+          dataCacheConnector.save[$className$](request.sessionId, $className$Id.toString, value).map(cacheMap =>
+            Redirect(navigator.nextPage($className$Id)(new UserAnswers(cacheMap))))
+      )
+  }
+}
