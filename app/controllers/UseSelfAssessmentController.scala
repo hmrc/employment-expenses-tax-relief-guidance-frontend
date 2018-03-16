8445ccdbac19cbdb15581977e9a8135d43d91d68
Nick Wilson <thenickwilson@gmail.com>
2018-03-16 08:26:11 +0000

    Pass claimaint into to changed views

--- app/controllers/UseSelfAssessmentController.scala
+++ app/controllers/UseSelfAssessmentController.scala
@@ -27,12 +27,13 @@ import views.html.useSelfAssessment
 import scala.concurrent.Future
 
 class UseSelfAssessmentController @Inject()(appConfig: FrontendAppConfig,
-                                         override val messagesApi: MessagesApi,
-                                         getData: DataRetrievalAction,
-                                         requireData: DataRequiredAction) extends FrontendController with I18nSupport {
+                                            override val messagesApi: MessagesApi,
+                                            getData: DataRetrievalAction,
+                                            requireData: DataRequiredAction,
+                                            getClaimant: GetClaimantAction) extends FrontendController with I18nSupport {
 
-  def onPageLoad = (getData andThen requireData) {
+  def onPageLoad = (getData andThen requireData andThen getClaimant) {
     implicit request =>
-      Ok(useSelfAssessment(appConfig))
+      Ok(useSelfAssessment(appConfig, request.claimant))
   }
 }
