d839ab972457f96944404498e87bd50dc66993e9
garethgooch <31101509+garethgooch@users.noreply.github.com>
2019-07-09 17:24:59 +0100

    EE-784: removed filters and update some dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,13 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.0-play26",
     "uk.gov.hmrc" %% "logback-json-logger"           % "4.4.0",
-    "uk.gov.hmrc" %% "govuk-template"                % "5.33.0-play-26",
-    "uk.gov.hmrc" %% "play-health"                   % "3.14.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template"                % "5.36.0-play-26",
+    "uk.gov.hmrc" %% "play-health"                   % "3.12.0-play-26",
     "uk.gov.hmrc" %% "play-ui"                       % "7.40.0-play-26",
-    "uk.gov.hmrc" %% "http-caching-client"           % "8.4.0-play-25",
+    "uk.gov.hmrc" %% "http-caching-client"           % "8.4.0-play-26",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "0.2.0",
-    "uk.gov.hmrc" %% "bootstrap-play-26"             % "0.37.0",
-    "uk.gov.hmrc" %% "play-language"                 % "3.4.0",
-    "uk.gov.hmrc" %% "play-whitelist-filter"         % "2.0.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26"             % "0.42.0",
+    "uk.gov.hmrc" %% "play-language"                 % "4.0.0",
     "uk.gov.hmrc" %% "tax-year"                      % "0.5.0"
 
   )
