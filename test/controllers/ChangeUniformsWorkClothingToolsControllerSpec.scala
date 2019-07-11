6e3e3c72000be76edb6cd179581353ae6bf29479
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2019-07-11 14:56:28 +0100

    EE-784: update spec for MorethanFive Jobs and EmployerPaidBack Controllers Specs

--- test/controllers/ChangeUniformsWorkClothingToolsControllerSpec.scala
+++ test/controllers/ChangeUniformsWorkClothingToolsControllerSpec.scala
@@ -22,7 +22,8 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import utils.{FakeNavigator, Navigator}
 
-class ChangeUniformsWorkClothingToolsControllerSpec extends SpecBase {
+class
+ChangeUniformsWorkClothingToolsControllerSpec extends SpecBase {
 
   def onwardRoute = routes.IndexController.onPageLoad()
 
