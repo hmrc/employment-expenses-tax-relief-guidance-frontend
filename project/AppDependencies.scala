cefd769e2c452e166f096c48f449166f35b98f73
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2020-02-03 13:02:42 +0000

    DL-2680: Switched to use simple-reactivemongo, overidden dependency to fix qa deployment issue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo"   %% "play2-reactivemongo"           % "0.20.2-play26",
+    "uk.gov.hmrc"         %% "simple-reactivemongo"          % "7.23.0-play-26",
     "uk.gov.hmrc"         %% "logback-json-logger"           % "4.6.0",
     "uk.gov.hmrc"         %% "govuk-template"                % "5.48.0-play-26",
     "uk.gov.hmrc"         %% "play-health"                   % "3.14.0-play-26",
