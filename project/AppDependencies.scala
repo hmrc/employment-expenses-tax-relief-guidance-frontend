7302986ff05e86d14018e8fbcabb6575e4f15a3c
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2020-03-06 08:54:19 +0000

    dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,23 +8,22 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"         %% "simple-reactivemongo"          % "7.23.0-play-26",
     "uk.gov.hmrc"         %% "logback-json-logger"           % "4.6.0",
-    "uk.gov.hmrc"         %% "govuk-template"                % "5.48.0-play-26",
+    "uk.gov.hmrc"         %% "govuk-template"                % "5.52.0-play-26",
     "uk.gov.hmrc"         %% "play-health"                   % "3.14.0-play-26",
-    "uk.gov.hmrc"         %% "play-ui"                       % "8.7.0-play-26",
+    "uk.gov.hmrc"         %% "play-ui"                       % "8.8.0-play-26",
     "uk.gov.hmrc"         %% "http-caching-client"           % "9.0.0-play-26",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping" % "1.2.0-play-26",
-    "uk.gov.hmrc"         %% "bootstrap-play-26"             % "1.3.0",
+    "uk.gov.hmrc"         %% "bootstrap-play-26"             % "1.5.0",
     "uk.gov.hmrc"         %% "play-language"                 % "4.2.0-play-26",
     "uk.gov.hmrc"         %% "tax-year"                      % "1.0.0",
     "com.typesafe.play"   %% "play-iteratees"                % "2.6.1"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"     % "1.3.0"  classifier "tests",
     "org.scalatest"           %% "scalatest"             % "3.0.8",
     "org.scalatestplus.play"  %% "scalatestplus-play"    % "3.1.3",
     "org.pegdown"             %  "pegdown"               % "1.6.0",
-    "org.jsoup"               %  "jsoup"                 % "1.12.1",
+    "org.jsoup"               %  "jsoup"                 % "1.13.1",
     "com.typesafe.play"       %% "play-test"             % PlayVersion.current,
     "org.mockito"             %  "mockito-all"           % "1.10.19",
     "org.scalacheck"          %% "scalacheck"            % "1.14.3"
