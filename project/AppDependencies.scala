15c3cfe64915b9431cdb51bfb128bf2049841bb4
danny-waughman <daniel.waughman@digital.hmrc.gov.uk>
2023-12-07 10:01:14 +0000

    DL-12322 Updating play-frontend to accomodate HMRC Welsh wording change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"            % "1.3.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"    % bootstrapVersion,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"            % "7.19.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"            % "7.29.0-play-28",
     "uk.gov.hmrc"         %% "tax-year"                      % "3.3.0"
   )
 
