09f609e37c9404352bfccf3c94c84676cc8cda22
kwame-f-manso-acc <171707914+kwame-f-manso-acc@users.noreply.github.com>
2024-10-15 17:02:37 +0100

    DL-14677: updated jounery route and completed print and post page based on fs

--- app/utils/Navigator.scala
+++ app/utils/Navigator.scala
@@ -118,7 +118,7 @@ class Navigator @Inject()(implicit appConfig: FrontendAppConfig) {
     case (Some(Claimant.You), _) if isClaimingWfh(userAnswers) =>
       routes.DisclaimerController.onPageLoad()
     case (Some(Claimant.You), Some(_)) =>
-      routes.PaidTaxInRelevantYearController.onPageLoad()
+      routes.WillPayTaxController.onPageLoad() //is this depdent on the FS
     case (Some(Claimant.SomeoneElse), _) =>
       routes.UsePrintAndPostController.printAndPostGuidance()
     case _ =>
