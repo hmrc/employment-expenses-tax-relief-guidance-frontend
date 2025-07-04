f1a2b68ddbfdbbd0546704f9256c4abb94e64454
Mathew Joseph <mathew.a.joseph@accenture.com>
2025-07-04 17:00:33 +0100

    DL-16800: Enabled rebrand feature switch (#343)
    * DL-16800: Enabled rebrand feature switch
    
    * DL-16800: Reset pega journey switch to true

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.6.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.7.0",
     "uk.gov.hmrc"       %% "tax-year"                              % "5.0.0"
   )
 
