cf5c54c62149f4c4d167681890b8cd3fc57e36a1
geethumathew <geethuelizebeth@gmail.com>
2026-02-19 14:13:40 +0000

    DL-18511:Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,14 @@
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "10.4.0"
+  val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.10.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.12.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
     "uk.gov.hmrc"       %% "tax-year"                              % "6.0.0"
   )
 
